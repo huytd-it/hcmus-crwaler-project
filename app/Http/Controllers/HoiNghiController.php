9d8ccc84e55f86c3d1a6243a4e5027ff1d77e174
huytd-it <yukihuy9999@gmail.com>
2023-12-01 00:01:18 +0700

    convert data to JSON

--- app/Http/Controllers/HoiNghiController.php
+++ app/Http/Controllers/HoiNghiController.php
@@ -5,16 +5,34 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 use Symfony\Component\DomCrawler\Crawler;
-
+use Illuminate\Support\Str;
 class HoiNghiController extends Controller
 {
     function index()  {
         $host = 'https://vinasa.org.vn/';
         $response = Http::withoutVerifying()->get('https://vinasa.org.vn/vinasa/4/3074/4205/lich-su-kien/');
         $crawler = new Crawler($response);
-        foreach($crawler->filter('.ListItem > .Item') as $value){
-            echo($value);
-        }
+
+        $nodeValues = $crawler->filter('.ListItem > .Item')->each(function (Crawler $card, $i) {
+
+
+            $dateTime = explode(" | ",$card->filter('.DateEvent')->innerText());
+
+
+            $date = explode(" - ",$dateTime[0]);
+            // $time = explode(" đến ", $dateTime[1]);
+
+
+            return [
+                'ten_hoi_nghi' => $card->filter('.TitleEvent')->text(),
+                'thoi_gian_bat_dau' => isset($date[0]) ? $date[0] : NULL,
+                'thoi_gian_ket_thuc' => isset($date[1]) ? $date[1] : NULL,
+                'dia_diem' => $card->filter('.AddressEvent')->innerText(),
+            ];
+        });
+
+       // var_dump($nodeValues);
+        return response($nodeValues);
 
     }
 }
